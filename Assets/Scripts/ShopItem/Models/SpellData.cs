1ed4066d87597c7e6017008d9c1b09a71dd572a8
Masood Taheri <masoud.taheri@FinitX.com>
2025-07-17 19:27:51 +0330

    implement different shop item

---
+++ Assets/Scripts/ShopItem/Models/SpellData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Spell", menuName = "Shop/Spell")]
+
+public class SpellData : ItemBaseData
+{
+ 
+}
---
+++ Assets/Scripts/ShopItem/Models/ToolData.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Tool", menuName = "Shop/Tool")]
+
+public class ToolData : ItemBaseData
+{
+ 
+}
