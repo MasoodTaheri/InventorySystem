a6425af24d504f9d9633e7606af61cfcf34e83ba
Masood Taheri <masoud.taheri@FinitX.com>
2025-07-17 19:27:30 +0330

    shop item base class as scriptable data

---
+++ Assets/Scripts/ShopItem/Models/ItemData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ItemBaseData : ScriptableObject
+{
+    public int id;
+    public string itemName;
+    public Sprite icon;
+    public string description;
+    public int price;
+}
