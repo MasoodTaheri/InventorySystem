ed829fb3bee2348c8c935d974aedda5905516fc5
Masood Taheri <masoud.taheri@FinitX.com>
2025-07-18 11:47:46 +0330

    Save system

---
+++ Assets/ItemSaveSystem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public static class ItemSaveSystem
+{
+    private const string SAVE_KEY = "PlayerItems";
+
+    public static void SaveItems(List<ItemCount> items)
+    {
+        string json = JsonUtility.ToJson(new ItemListWrapper(items));
+        PlayerPrefs.SetString(SAVE_KEY, json);
+        PlayerPrefs.Save(); 
+    }
+
+    public static List<ItemCount> LoadItems()
+    {
+        if (PlayerPrefs.HasKey(SAVE_KEY))
+        {
+            string json = PlayerPrefs.GetString(SAVE_KEY);
+            return JsonUtility.FromJson<ItemListWrapper>(json).items;
+        }
+        return new List<ItemCount>(); 
+    }
+
+    [System.Serializable]
+    private class ItemListWrapper
+    {
+        public List<ItemCount> items;
+        public ItemListWrapper(List<ItemCount> items) => this.items = items;
+    }
+
+    public static void UpdateItem(string itemId, int addCount)
+    {
+        var items = LoadItems();
+        var existing = items.FirstOrDefault(x => x.itemId == itemId);
+
+        if (existing != null)
+        {
+            existing.count += addCount;
+        }
+        else
+        {
+            items.Add(new ItemCount(itemId, addCount));
+        }
+
+        SaveItems(items);
+    }
+}
