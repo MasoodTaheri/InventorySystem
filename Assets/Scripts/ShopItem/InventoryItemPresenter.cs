fc4961dd6ef82cead568c41bb57ebb32dc449ad7
Masood Taheri <masoud.taheri@FinitX.com>
2025-07-18 11:47:02 +0330

    inventory MVP

---
+++ Assets/Scripts/ShopItem/InventoryItemPresenter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class InventoryItemPresenter:ItemPresenter
+{
+    [SerializeField] protected InventoryItemView _view;
+    public void Initialize(ItemBaseData model, int count)
+    {
+        _model = model;
+        _view.InventoryInitialize(_model, count);
+    }
+}
---
+++ Assets/Scripts/ShopItem/InventoryItemView.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class InventoryItemView : ItemView
+{
+    [SerializeField] private TMP_Text _count;
+
+    public void InventoryInitialize(ItemBaseData data, int count)
+    {
+        Initialize(data);
+        _count.text = count.ToString();
+
+    }
+}
